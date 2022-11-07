d17d2d07dab083399cd0674b1c65f53d457b288f
Jareth Gomes <jarethgomes@gmail.com>
2022-11-06 23:26:22 -0600

    Added setup code for loadcell

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <Arduino.h>
+#include "HX711.h"
+
+HX711 loadcell;
+
+// 1. HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 2;
+const int LOADCELL_SCK_PIN = 3;
+
+// 2. Adjustment settings
+const long LOADCELL_OFFSET = 50682624;
+const long LOADCELL_DIVIDER = 5895655;
 
 void setup() {
-  // put your setup code here, to run once:
+  loadcell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  loadcell.set_scale(LOADCELL_DIVIDER);
+  loadcell.set_offset(LOADCELL_OFFSET);
 }
 
 void loop() {
