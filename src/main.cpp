c15a09de5e30b5bbe4d94f2e1d01e6a3230ebc06
Jareth Gomes <jarethgomes@gmail.com>
2022-11-29 00:19:04 -0600

    Changed indents to 4 spaces from 2

--- src/main.cpp
+++ src/main.cpp
@@ -37,173 +37,173 @@ RunningNumbers<float> weights(10);
 static bool weighing_completed  = false;
 
 static PT_THREAD(led_gradual_pulse (struct pt *pt)) {
-  PT_BEGIN(pt);
+    PT_BEGIN(pt);
 
-  #ifdef DEBUG
-  if (!weighing_completed)
-    Serial.println("Began fade step");
-  #endif
-
-  led.set_color(RGBB{255, 150, 0, 0}); // Orange color
-
-  while (!weighing_completed) {
     #ifdef DEBUG
-    Serial.println("LED Step");
+    if (!weighing_completed)
+        Serial.println("Began fade step");
     #endif
-    led.fading_brightness_step();
-    PT_SLEEP(pt, 10);
-  }
 
-  led.reset_and_apply();
-  PT_END(pt);
+    led.set_color(RGBB{255, 150, 0, 0}); // Orange color
+
+    while (!weighing_completed) {
+        #ifdef DEBUG
+        Serial.println("LED Step");
+        #endif
+        led.fading_brightness_step();
+        PT_SLEEP(pt, 10);
+    }
+
+    led.reset_and_apply();
+    PT_END(pt);
 }
 
 static struct pt measure_tare_pt;
 
 static PT_THREAD(measure_tare (struct pt *pt)) {
-  static struct pt led_pulse_pt;
+    static struct pt led_pulse_pt;
 
-  PT_BEGIN(pt);
+    PT_BEGIN(pt);
 
-  weighing_completed = false;
+    weighing_completed = false;
 
-  Serial.println("Calibrating tare weight ...");
+    Serial.println("Calibrating tare weight ...");
 
-  PT_INIT(&led_pulse_pt);
-  PT_SCHEDULE(led_gradual_pulse(&led_pulse_pt));
-
-  // From HX711::read_average()
-  // TODO: Is there a nicer way of incorporating protothreads into a separate function? (probably
-  // since PT_SCHEDULE doesnt do much other than call the function directly)
-  long sum = 0;
-  const int times = 100;
-	for (byte i = 0; i < times; i++) {
-    #ifdef DEBUG
-    Serial.println("Reading loadcell");
-    delayMicroseconds(random(10, 5000));
-    #endif
-		sum += loadcell.read();
-		delay(0);
+    PT_INIT(&led_pulse_pt);
     PT_SCHEDULE(led_gradual_pulse(&led_pulse_pt));
-	}
-	long avg = sum / times;
-	loadcell.set_offset(avg);
 
-  weighing_completed = true;
-  PT_WAIT_THREAD(pt, led_gradual_pulse(&led_pulse_pt));
+    // From HX711::read_average()
+    // TODO: Is there a nicer way of incorporating protothreads into a separate function? (probably
+    // since PT_SCHEDULE doesnt do much other than call the function directly)
+    long sum = 0;
+    const int times = 100;
+        for (byte i = 0; i < times; i++) {
+        #ifdef DEBUG
+        Serial.println("Reading loadcell");
+        delayMicroseconds(random(10, 5000));
+        #endif
+            sum += loadcell.read();
+            delay(0);
+        PT_SCHEDULE(led_gradual_pulse(&led_pulse_pt));
+        }
+        long avg = sum / times;
+        loadcell.set_offset(avg);
 
-  PT_END(pt);
+    weighing_completed = true;
+    PT_WAIT_THREAD(pt, led_gradual_pulse(&led_pulse_pt));
+
+    PT_END(pt);
 }
 
 static PT_THREAD(measure_weight (struct pt *pt)) {
-  static struct pt led_pulse_pt;
+    static struct pt led_pulse_pt;
 
-  PT_BEGIN(pt);
+    PT_BEGIN(pt);
 
-  weighing_completed = true;
-  PT_INIT(&led_pulse_pt);
+    weighing_completed = true;
+    PT_INIT(&led_pulse_pt);
 
-  for (size_t i = 0; i < weights.get_max_elements(); i++) {
-    weights.add(loadcell.get_units());
-  }
+    for (size_t i = 0; i < weights.get_max_elements(); i++) {
+        weights.add(loadcell.get_units());
+    }
 
-  while (true) {
-    if (weights.get_varience() <= VARIENCE_EPLSION && !weighing_completed) {
-      weighing_completed = true;
-      PT_WAIT_THREAD(pt, led_gradual_pulse(&led_pulse_pt));
+    while (true) {
+        if (weights.get_varience() <= VARIENCE_EPLSION && !weighing_completed) {
+        weighing_completed = true;
+        PT_WAIT_THREAD(pt, led_gradual_pulse(&led_pulse_pt));
 
-      // TODO: check if weight is within tolerance to 0
-      if (abs(weights.get_avg()) <= TOLERANCE) {
-        // CORRECT!
-        #ifdef DEBUG
-        Serial.println("Correct!");
-        #endif
-        led.set_color(RGBB{0, 255, 0, 100});
-        led.apply();
-        lock.release();
-        delay(30000);
-        break;
-      } else {
-        // INCORRECT!
-        #ifdef DEBUG
-        Serial.println("Incorrect!");
-        #endif
-        for (int i = 0; i < 3; i++) {
-          led.set_color(RGBB{255, 0, 0, 100});
-          led.apply();
-          delay(1000);
-          led.reset_and_apply();
-          delay(1000);
+        // TODO: check if weight is within tolerance to 0
+        if (abs(weights.get_avg()) <= TOLERANCE) {
+            // CORRECT!
+            #ifdef DEBUG
+            Serial.println("Correct!");
+            #endif
+            led.set_color(RGBB{0, 255, 0, 100});
+            led.apply();
+            lock.release();
+            delay(30000);
+            break;
+        } else {
+            // INCORRECT!
+            #ifdef DEBUG
+            Serial.println("Incorrect!");
+            #endif
+            for (int i = 0; i < 3; i++) {
+            led.set_color(RGBB{255, 0, 0, 100});
+            led.apply();
+            delay(1000);
+            led.reset_and_apply();
+            delay(1000);
+            }
         }
-      }
+        }
+
+        // TODO: replace with function if posible to reduce redundant code
+        long sum = 0;
+        const int times = 10;
+        for (byte i = 0; i < times; i++) {
+        #ifdef DEBUG
+        Serial.println("Reading loadcell");
+        delayMicroseconds(random(10, 5000));
+        #endif
+        // sum += 0; // random(1000, 10000);
+        sum += loadcell.read();
+        delay(0);
+        PT_SCHEDULE(led_gradual_pulse(&led_pulse_pt));
+        }
+        long avg = sum / times - loadcell.get_offset();
+        weights.add(avg / loadcell.get_scale());
+
+        if (weights.get_varience() > VARIENCE_EPLSION) {
+        weighing_completed = false;
+        }
     }
 
-    // TODO: replace with function if posible to reduce redundant code
-    long sum = 0;
-    const int times = 10;
-    for (byte i = 0; i < times; i++) {
-      #ifdef DEBUG
-      Serial.println("Reading loadcell");
-      delayMicroseconds(random(10, 5000));
-      #endif
-      // sum += 0; // random(1000, 10000);
-      sum += loadcell.read();
-      delay(0);
-      PT_SCHEDULE(led_gradual_pulse(&led_pulse_pt));
-    }
-    long avg = sum / times - loadcell.get_offset();
-    weights.add(avg / loadcell.get_scale());
-
-    if (weights.get_varience() > VARIENCE_EPLSION) {
-      weighing_completed = false;
-    }
-  }
-
-  led.reset_and_apply();
-  PT_END(pt);
+    led.reset_and_apply();
+    PT_END(pt);
 }
 
 void setup() {
-  Serial.begin(9600);
-  PT_INIT(&measure_tare_pt);
+    Serial.begin(9600);
+    PT_INIT(&measure_tare_pt);
 
-  loadcell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  loadcell.set_scale(LOADCELL_DIVIDER);
+    loadcell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+    loadcell.set_scale(LOADCELL_DIVIDER);
 
-  // Change LED to indicate the scale is powered on
-  led.set_color(RGBB{255, 255, 255, 100}); // turn LED white
-  led.apply();
-  delay(1000);
-
-  // pulse LED blue in 1-second cycles for 3 seconds
-  constexpr int PRE_TARE_SECS = 3;
-  for (int i = 0; i < PRE_TARE_SECS; i++) {
-    led.set_color(RGBB{0, 0, 255, 100});
+    // Change LED to indicate the scale is powered on
+    led.set_color(RGBB{255, 255, 255, 100}); // turn LED white
     led.apply();
-    delay(500);
-    led.set_brightness(0);
+    delay(1000);
+
+    // pulse LED blue in 1-second cycles for 3 seconds
+    constexpr int PRE_TARE_SECS = 3;
+    for (int i = 0; i < PRE_TARE_SECS; i++) {
+        led.set_color(RGBB{0, 0, 255, 100});
+        led.apply();
+        delay(500);
+        led.set_brightness(0);
+        led.apply();
+    }
+
+    Serial.println("Calibrating ...");
+
+    // Call the protothread routine to measure the tare weight
+    // This should also make the LED gradually pulse orange every 1 second or so
+    PT_SCHEDULE(measure_tare(&measure_tare_pt));
+
+    // Set the LED color to green to signal it is done calibraing
+    led.set_color(RGBB{0, 255, 0, 100});
     led.apply();
-  }
-
-  Serial.println("Calibrating ...");
-
-  // Call the protothread routine to measure the tare weight
-  // This should also make the LED gradually pulse orange every 1 second or so
-  PT_SCHEDULE(measure_tare(&measure_tare_pt));
-
-  // Set the LED color to green to signal it is done calibraing
-  led.set_color(RGBB{0, 255, 0, 100});
-  led.apply();
-  Serial.println("Done calibrating!");
-  delay(5000); // Wait 5 seconds to allow for weights to be taken off the scale
-  led.reset_and_apply();
-  Serial.println("Ready and actively waiting for correct weights!");
-  // Once LED is off, the scale is operational
+    Serial.println("Done calibrating!");
+    delay(5000); // Wait 5 seconds to allow for weights to be taken off the scale
+    led.reset_and_apply();
+    Serial.println("Ready and actively waiting for correct weights!");
+    // Once LED is off, the scale is operational
 }
 
 void loop() {
-  static struct pt measure_pt;
-  PT_INIT(&measure_pt);
-  PT_SCHEDULE(measure_weight(&measure_pt));
-  exit(0);
+    static struct pt measure_pt;
+    PT_INIT(&measure_pt);
+    PT_SCHEDULE(measure_weight(&measure_pt));
+    exit(0);
 }
